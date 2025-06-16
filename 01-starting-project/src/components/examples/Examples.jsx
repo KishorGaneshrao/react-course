3d8694b9772ef2ed81078f50dee42661ccf072cf
KGaneshrao <kganeshrao@linz.govt.nz>
2025-06-16 13:26:16 +1200

    splitting component to have multiple jsx slots as part of tabs component

--- 01-starting-project/src/components/examples/Examples.jsx
+++ 01-starting-project/src/components/examples/Examples.jsx
@@ -2,27 +2,29 @@ import { useState } from "react";
 import TabButton from "../tabButton/TabButton";
 import TopicContent from "../topicContent/TopicContent";
 import Section from "../section/Section";
+import Tabs from "../tabs/Tabs";
 
 export default function Examples() {
   const topics = ["components", "jsx", "props", "state"];
   const [selectedTopic, setSelectedTopic] = useState();
 
+  const buttons = topics.map((topic, index) => {
+    return (
+      <TabButton
+        key={index}
+        isActive={selectedTopic === topic}
+        onClick={() => setSelectedTopic(topic)}
+      >
+        {topic}
+      </TabButton>
+    );
+  });
+
   return (
     <Section title={"Examples"} id="examples">
-      <menu>
-        {topics.map((topic, index) => {
-          return (
-            <TabButton
-              key={index}
-              isActive={selectedTopic === topic}
-              onClick={() => setSelectedTopic(topic)}
-            >
-              {topic}
-            </TabButton>
-          );
-        })}
-      </menu>
-      <TopicContent topic={selectedTopic} />
+      <Tabs buttons={buttons}>
+        <TopicContent topic={selectedTopic} />
+      </Tabs>
     </Section>
   );
 }
